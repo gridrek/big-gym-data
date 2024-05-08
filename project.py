e79f72911cf205f6b722e3409a8a713e43822024
gridrek <gabinus_93@hotmail.com>
2024-05-08 15:19:13 +0200

    testing testing

--- project.py
+++ project.py
@@ -3,3 +3,8 @@ import matplotlib.pyplot as plt
 
 data_csv = "gym_sensor_data_2023-04-02_to_2023-04-09.csv"
 
+data = pd.read_csv("./" + data_csv, sep=";", skiprows=0)
+
+print(data.head(n=10))
+
+data.plot(x="timestamp", y="machine_1", title="usage") #det här funkar inte för fan
